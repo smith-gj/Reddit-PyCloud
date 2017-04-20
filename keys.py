44a13dba2b2541da7990b5d0840e20fc92a39cbe
isaactm98 <isaactm98@gmail.com>
2017-04-20 15:35:32 -0400

    Update
    added get functions

--- keys.py
+++ keys.py
@@ -1,8 +1,14 @@
 # Enter your information here
 
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-USER_AGENT = "Reddit PyCloud bot v1.0 (by /u/botManCloud)"
+CLIENT_ID = 'FWUUffoDS-TO3Q'
+CLIENT_SECRET = 'ntsPeOVHFN3Vdlb_F-2J2-JJiqw'
+USER_AGENT = 'Reddit PyCloud bot v1.0 (by /u/botManCloud)'
 
-def getKeys():
-    return (CLIENT_ID, CLIENT_SECRET, USER_AGENT)
+def getID():
+    return (CLIENT_ID)
+
+def getSecret():
+    return (CLIENT_SECRET)
+
+def getAgent():
+    return (USER_AGENT)
