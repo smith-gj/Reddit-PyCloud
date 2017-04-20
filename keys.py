f09a43889b5c57f9e83ef62271ec87040036a3e7
smith-gj <smith.geb42@gmail.com>
2017-04-20 15:30:06 -0400

    Update keys.py
    Fixed syntax

--- keys.py
+++ keys.py
@@ -1,8 +1,8 @@
 # Enter your information here
 
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-USER_AGENT = "Reddit PyCloud bot v1.0 (by /u/botManCloud)"
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+USER_AGENT = 'Reddit PyCloud bot v1.0 (by /u/botManCloud)'
 
 def getKeys():
     return (CLIENT_ID, CLIENT_SECRET, USER_AGENT)
