18a1489bada2782004846d465c410a65e911980f
smith-gj <smith.geb42@gmail.com>
2017-04-24 13:29:21 -0400

    Updated feedBot and added snoo.png
    PNG acts as mask for wordcloud, feedBot adjusted to compensate

---
+++ feedBot.py
@@ -0,0 +1,42 @@
+# Code based off masked example from https://amueller.github.io/word_cloud
+import numpy as np
+from PIL import Image
+from os import path
+import matplotlib.pyplot as plt
+import random
+
+from wordcloud import WordCloud, STOPWORDS
+
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+
+d = path.dirname(__file__)
+
+# snoo image for wordcloud mask
+# http://reddits-world.wikia.com/wiki/Snoo
+mask = np.array(Image.open(path.join(d, "snoo.png")))
+
+# Read text file of scraped reddit data from the API
+text = open("results.txt").read()
+
+# adding movie script specific stopwords
+stopwords = set(STOPWORDS)
+stopwords.add("int")
+stopwords.add("ext")
+
+wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
+               random_state=1).generate(text)
+# store default colored image
+default_colors = wc.to_array()
+plt.title("Custom colors")
+plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3),
+           interpolation="bilinear")
+wc.to_file("reddit-cloud.png")
+plt.axis("off")
+plt.figure()
+plt.title("Default colors")
+plt.imshow(default_colors, interpolation="bilinear")
+plt.axis("off")
+plt.show()
