7c48f5912d31008428bd406fd1a6752195c55a18
isaactm98 <isaactm98@gmail.com>
2017-04-24 23:24:02 -0400

    Can retrieve comments
    Added code that takes in a topic and subreddit and will output most
    comments into a txt file from daily top posts that match the keyword
    given by the user.

--- api.py
+++ api.py
@@ -1,6 +1,48 @@
 import praw
 from keys import getID, getSecret, getAgent
 
-reddit = praw.Reddit(client_id= getID,
-                     client_secret=getSecret,
-                     user_agent=getAgent)
+def main():
+    #create reddit instance
+    ID = getID()
+    secret = getSecret()
+    agent = getAgent()
+    reddit = praw.Reddit(client_id= ID,
+                         client_secret=secret,
+                         user_agent=agent)
+    #get keyword
+    keyword = getKeyword()
+    #get subreddit
+    subreddit = getSubreddit()
+    #create file necessary
+    file_name = subreddit + ".txt"
+    new_file = open(file_name, "w", encoding="utf8")
+    #gets top 100 posts from day http://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
+    for submission in reddit.subreddit(subreddit).top('day'):
+        process_submission(submission, keyword, new_file)
+    new_file.close()
+
+#also used praw documentation to help this
+def process_submission(submission, keyword, new_file):
+    normalized_title = submission.title.lower()
+    #should get most comments from thread and add to new_file
+    if keyword in normalized_title:
+        #written with assistance from documentation http://praw.readthedocs.io/en/latest/tutorials/comments.html
+        submission.comments.replace_more(limit=0)
+        #convert to string
+        for comment in submission.comments.list():
+            text = str(comment.body)
+            #write to file
+            new_file.write(text)
+            new_file.write(" ")
+
+def getKeyword():
+    #get keyword
+    keyword = input('Enter topic keyword: ')
+    #convert to lowercase to supplement later string comparison
+    keyword = keyword.lower()
+    return keyword
+
+def getSubreddit():
+    #get subreddit
+    name = input('Enter subreddit name: ')
+    return name
---
+++ main.py
@@ -0,0 +1,4 @@
+#what the user will run to activate program
+from api import main
+
+main()
